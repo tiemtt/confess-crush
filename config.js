48410d4e206153c9cfe58343f27c11447446e759
tiemtt <144120258+tiemtt@users.noreply.github.com>
2023-09-05 17:59:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "này cậu ơi",
+    introTitle: 'tớ có điều muốn nói',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
